160dcf626b40d0c3ce011898a169aadf10decfc0
John Bell <jbellny@gmail.com>
2025-02-24 10:24:26 -0500

    fixed binding error with force button

--- src/app/components/forceButton.tsx
+++ src/app/components/forceButton.tsx
@@ -1,4 +1,11 @@
-export default function ForceButton({children, align = "start"}) {
+import  React, { ReactNode } from "react"
+
+interface ForceButtonProps {
+  children: ReactNode;
+  align?: "start" | "end" | "center";
+}
+
+export default function ForceButton({children, align = "start"}: ForceButtonProps ) {
   return (
     <button className={`bg-[#2E3091] text-white/90 w-fit px-4 py-2 uppercase font-inter font-semibold rounded-md mt-auto self-${align}`}>
       {children}
